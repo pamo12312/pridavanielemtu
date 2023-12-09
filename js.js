8ec53ead4134cdc4af092506557e583262174faf
Patrik <patri.moravec@email.cz>
2023-12-09 21:30:13 +0100

    vytvoření počáteční velikosti tabulky

--- js.js
+++ js.js
@@ -1,14 +1,28 @@
-const words = ["Petr", "Jan", "Honza", "Adam", "Marek", "Jirka", "Ondra", "Lucie", "Tereza", "Patrik", "Denis"];
+const words = ["Jirka", "Ondra", "Lucie", "Tereza", "Patrik", "Denis"];
 
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
+}
+
+function initializeTable() {
+    const table = document.querySelector("#myTable");
+
+    // Add initial 10 rows to the table
+    for (let i = 0; i < 10; i++) {
+        const row = table.insertRow();
+
+        for (let j = 0; j < 5; j++) {
+            const cell = row.insertCell();
+            cell.innerHTML = getRandomWord();
+        }
+    }
 }
 
 function addRow() {
     const table = document.querySelector("#myTable");
     const row = table.insertRow();
 
-    for (let j = 0; j < 3; j++) {
+    for (let j = 0; j < 5; j++) {
         const cell = row.insertCell();
         cell.innerHTML = getRandomWord();
     }
@@ -17,6 +31,7 @@ function addRow() {
 function deleteRow() {
     const table = document.querySelector("#myTable");
 
+    // Check if the table has more than 1 row before deleting the last row
     if (table.rows.length > 1) {
         table.deleteRow(-1);
     }
@@ -29,7 +44,10 @@ function reverseTable() {
 
     table.innerHTML = "";
 
-    rows.forEach(row => {
+    rows.forEach((row) => {
         table.appendChild(row);
     });
 }
+
+// Initialize the table when the page is loaded
+document.addEventListener("DOMContentLoaded", initializeTable);
